446ac895e06c2f916afc364b955ef09937b426cc
LDuve <hugo.duvi@gmail.com>
2023-04-04 16:05:26 +0200

    Implémentation pyshark pour 1ere sous question DNS

--- Capture.py
+++ Capture.py
@@ -1,5 +1,19 @@
 import pyshark as ps
 
 cap = ps.FileCapture('Capture_Trace_1.pcapng')
+DNSResolu = {}
+NvResolu = {}
 
-print(cap[0][1])
+for packet in cap:
+    if 'DNS' in packet:
+        domaine = packet.dns.qry_name
+
+        if domaine not in DNSResolu: 
+            DNSResolu[domaine] = packet.sniff_time.timestamp()
+            NvResolu[domaine] = packet.sniff_time.timestamp()
+        else:
+            NvResolu[domaine] = packet.sniff_time.timestamp()
+
+print(f"{len(NvResolu)} noms de domaine ont ete resolus.")
+for domaine, temps_dernier_resolu in NvResolu.items():
+    print(f"Le nom de domaine '{domaine}' a ete resolu pour la derniere fois a {temps_dernier_resolu}.")
