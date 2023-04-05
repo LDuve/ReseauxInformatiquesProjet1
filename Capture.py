4da37cfb85d3cde6bb6d091656bfb5de28d489f6
LDuve <hugo.duvi@gmail.com>
2023-04-05 15:46:34 +0200

    Couche réseau Sans les technique pour la NAT

--- Capture.py
+++ Capture.py
@@ -5,7 +5,9 @@ import datetime
 cap = ps.FileCapture('Capture_Trace_1.pcapng')
 DNSResolu = {}
 NvResolu = {}
-TypeDNS = []
+TypeDNS = {}
+AddressDst = {}
+AddressSrc = {}
 AddRecords = []
 
 for packet in cap:
@@ -13,24 +15,40 @@ for packet in cap:
         domaine = packet.dns.qry_name
 
         if hasattr(packet.dns, 'an_count') and int(packet.dns.an_count) > 0:
-            # Extraire les enregistrements additionnels
             additional_records = packet.dns._all_fields[-1].showname.split()[1:]
             AddRecords.append(additional_records)
-
+        
+        time = packet.sniff_time.timestamp()
+        dt = datetime.datetime.fromtimestamp(time)
+        formatted_date = dt.strftime("%d-%m-%Y %H:%M:%S")
         if domaine not in DNSResolu : 
-            time = packet.sniff_time.timestamp()
-            dt = datetime.datetime.fromtimestamp(time)
-            formatted_date = dt.strftime("%d-%m-%Y %H:%M:%S")
-
-            DNSResolu[domaine] = formatted_date
+            DNSResolu[domaine]= formatted_date
             NvResolu[domaine] = formatted_date
-            query_type = packet.dns.qry_type
-            TypeDNS.append(query_type)
+            TypeDNS[domaine] = packet.dns.qry_type
+            if 'ipv6' in packet:
+                AddressSrc[domaine] = packet.ipv6.src
+                AddressDst[domaine] = packet.ipv6.dst
+            elif 'ipv4' in packet:
+                NvResolu[domaine] = packet.ipv4.src
+                AddressDst[domaine] = packet.ipv4.dst
+            elif 'ip' in packet:
+                NvResolu[domaine] = packet.ip.src
+                AddressDst[domaine] = packet.ip.dst
+       
         else:
-            NvResolu[domaine] = formatted_date
+            NvResolu[domaine]= formatted_date
+
+        
+        
 
 print(f"{len(NvResolu)} noms de domaine ont ete resolus.")
 for domaine, temps_dernier_resolu in NvResolu.items():
     print(f"Le nom de domaine '{domaine}' a ete resolu pour la derniere fois le {temps_dernier_resolu}.")
-print(f"les differents type DNS utilises sont {TypeDNS}.")
+
+print(f"Les differents type DNS utilises sont {TypeDNS}.")
 print(f"Les records additionnels sont {AddRecords}.")
+
+for domaine, destination in AddressDst.items():
+    print(f"Les paquets sont envoyes a l'adresse {destination} pour le domaine {domaine}.")
+
+
