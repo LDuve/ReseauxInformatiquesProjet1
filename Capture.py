8cacbfcd1ceb8ccdbea302104401831b73018f17
LDuve <hugo.duvi@gmail.com>
2023-04-05 18:23:18 +0200

    Script + Partie transport sans ext dans handshake

--- Capture.py
+++ Capture.py
@@ -10,8 +10,12 @@ AddressDst = {}
 AddressSrc = {}
 AddRecords = []
 
+
 for packet in cap:
+
     if 'DNS' in packet:
+        NetworkLayerName = packet.layers[1].layer_name
+        TransportLayerName = packet.layers[2].layer_name
         domaine = packet.dns.qry_name
 
         if hasattr(packet.dns, 'an_count') and int(packet.dns.an_count) > 0:
@@ -25,16 +29,8 @@ for packet in cap:
             DNSResolu[domaine]= formatted_date
             NvResolu[domaine] = formatted_date
             TypeDNS[domaine] = packet.dns.qry_type
-            if 'ipv6' in packet:
-                AddressSrc[domaine] = packet.ipv6.src
-                AddressDst[domaine] = packet.ipv6.dst
-            elif 'ipv4' in packet:
-                NvResolu[domaine] = packet.ipv4.src
-                AddressDst[domaine] = packet.ipv4.dst
-            elif 'ip' in packet:
-                NvResolu[domaine] = packet.ip.src
-                AddressDst[domaine] = packet.ip.dst
-       
+            AddressSrc[domaine] = packet.layers[1].src
+            AddressDst[domaine] = packet.layers[1].dst 
         else:
             NvResolu[domaine]= formatted_date
 
@@ -43,12 +39,55 @@ for packet in cap:
 
 print(f"{len(NvResolu)} noms de domaine ont ete resolus.")
 for domaine, temps_dernier_resolu in NvResolu.items():
-    print(f"Le nom de domaine '{domaine}' a ete resolu pour la derniere fois le {temps_dernier_resolu}.")
+    print(f"Le nom de domaine {domaine} a ete resolu pour la derniere fois le {temps_dernier_resolu}.")
 
-print(f"Les differents type DNS utilises sont {TypeDNS}.")
+for domaine, Typedns in TypeDNS.items():
+    print(f"Pour le domaine {domaine}, le type de DNS utilises est {Typedns}.")
+
 print(f"Les records additionnels sont {AddRecords}.")
 
 for domaine, destination in AddressDst.items():
     print(f"Les paquets sont envoyes a l'adresse {destination} pour le domaine {domaine}.")
+
+print("\n")
+
+#Partie transport
+TransportProtocole = []
+domain_ips = {}
+Quicversions = []
+OtherProto = []
+for packet in cap:
+    if 'QUIC' in packet:
+        Quicversions.append(packet.quic.version)
+
+    if 'DNS' in packet:
+        TransportProtocole.append(packet.layers[2].layer_name)  
+        domaine = packet.dns.qry_name
+        srcip = packet.layers[1].src
+
+
+        if domaine in domain_ips:
+            if srcip not in domain_ips[domaine]:
+                domain_ips[domaine].append(srcip)
+        else:
+            domain_ips[domaine] = [srcip]
+    if 'UDP' in packet:
+        if not 'DNS' in packet and not 'QUIC' in packet: 
+            for layer in packet.layers:
+                if layer.layer_name not in OtherProto:
+                    OtherProto.append(layer.layer_name)
+        
+        
+
+
+print(f"Le protocole de transport utilise sont {TransportProtocole}.")
+for domain, ips in domain_ips.items():
+    if len(ips) > 1:
+        print(f"Le domaine {domain} a ete contacte par plusieurs adresses IP sources : {ips}")
+
+print(f"Les versions de QUIC utlisee sont : {Quicversions}")
+print(f"Lorsque nous observons du trafic UDP, nous identifions aussi {OtherProto}")
+
+
 
 
